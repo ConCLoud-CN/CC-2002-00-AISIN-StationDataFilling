64d46665b94825f07f78f2558357ad10f420343b
leiWell <lei.zhou@con-cloud.cn>
2020-08-14 14:07:58 +0800

    First
    现场发布完成

---
+++ Test/Test/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Service
+    {
+        public double NextDouble(Random ran, double minValue, double maxValue, int decimalPlace)
+        {
+            double randNum = ran.NextDouble() * (maxValue - minValue) + minValue;
+            return Convert.ToDouble(randNum.ToString("f" + decimalPlace));
+        }
+    }
+}
